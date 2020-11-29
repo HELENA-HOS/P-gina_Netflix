b3e805c4c9a189a108adb77713438fdafdb4de86
HELENA-HOS <helena_oliveirasilva@yahoo.com.br>
2020-11-29 19:48:30 -0300

    Reprodução da Página do Netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function trailer(){
+    window.open("https://www.youtube.com/watch?v=GtA9ggWVoMc"); 
+}
+
+function info(){
+    window.open("https://pt.wikipedia.org/wiki/House_of_Cards_(s%C3%A9rie_americana)"); 
+}
